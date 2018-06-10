a5f7656e334d7016df790009953d77f6250dd866
An Nguyen <yasuosm@gmail.com>
2018-06-10 10:36:49 +0700

    Style image list

--- src/features/home/ImageList.js
+++ src/features/home/ImageList.js
@@ -9,106 +9,118 @@ export default class ImageList extends Component {
     return (
       <div className="home-image-list">
         <div className="row image-list">
-          <div className="col-md-3 image-item">
+          <div className="col-xs-6 col-sm-4 col-md-3 image-item">
             <div className="item-content">
-              <img src="http://lorempixel.com/100/100" alt="" />
+              <div className="item-img" style={{ backgroundImage: 'url(https://media2.giphy.com/media/bbs0P60ks0AZ1zOUhx/100.gif)' }} />
               <div className="item-stat">
                 <span className="pull-right">
-                  <span><i className="glyphicon glyphicon-eye-open" /> 7,693</span>
-                  <span><i className="glyphicon glyphicon-comment" /> 30</span>
-                  <span><i className="glyphicon glyphicon-heart" /> 901</span>
+                  <span className="stat"><i className="glyphicon glyphicon-eye-open" /> 7,693</span>
+                  <span className="stat"><i className="glyphicon glyphicon-comment" /> 30</span>
+                  <span className="stat"><i className="glyphicon glyphicon-heart" /> 901</span>
                 </span>
                 <i className="glyphicon glyphicon-paperclip" />
               </div>
             </div>
             <div className="item-user">
-              <img src="http://lorempixel.com/user/50/50" alt="" />
-              <a>John Doe</a>
+              <a className="user-link">
+                <img className="user-avatar" src="https://media.giphy.com/avatars/comedycentral/XCQBE9EqWbb2.jpg" alt="" />
+                <span>John Doe</span>
+              </a>
             </div>
           </div>
-          <div className="col-md-3 image-item">
+          <div className="col-xs-6 col-sm-4 col-md-3 image-item">
             <div className="item-content">
-              <img src="http://lorempixel.com/100/100" alt="" />
+              <div className="item-img" style={{ backgroundImage: 'url(https://media2.giphy.com/media/bbs0P60ks0AZ1zOUhx/100.gif)' }} />
               <div className="item-stat">
                 <span className="pull-right">
-                  <span><i className="glyphicon glyphicon-eye-open" /> 7,693</span>
-                  <span><i className="glyphicon glyphicon-comment" /> 30</span>
-                  <span><i className="glyphicon glyphicon-heart" /> 901</span>
+                  <span className="stat"><i className="glyphicon glyphicon-eye-open" /> 7,693</span>
+                  <span className="stat"><i className="glyphicon glyphicon-comment" /> 30</span>
+                  <span className="stat"><i className="glyphicon glyphicon-heart" /> 901</span>
                 </span>
                 <i className="glyphicon glyphicon-paperclip" />
               </div>
             </div>
             <div className="item-user">
-              <img src="http://lorempixel.com/user/50/50" alt="" />
-              <a>John Doe</a>
+              <a className="user-link">
+                <img className="user-avatar" src="https://media.giphy.com/avatars/comedycentral/XCQBE9EqWbb2.jpg" alt="" />
+                <span>John Doe</span>
+              </a>
             </div>
           </div>
-          <div className="col-md-3 image-item">
+          <div className="col-xs-6 col-sm-4 col-md-3 image-item">
             <div className="item-content">
-              <img src="http://lorempixel.com/100/100" alt="" />
+              <div className="item-img" style={{ backgroundImage: 'url(https://media2.giphy.com/media/bbs0P60ks0AZ1zOUhx/100.gif)' }} />
               <div className="item-stat">
                 <span className="pull-right">
-                  <span><i className="glyphicon glyphicon-eye-open" /> 7,693</span>
-                  <span><i className="glyphicon glyphicon-comment" /> 30</span>
-                  <span><i className="glyphicon glyphicon-heart" /> 901</span>
+                  <span className="stat"><i className="glyphicon glyphicon-eye-open" /> 7,693</span>
+                  <span className="stat"><i className="glyphicon glyphicon-comment" /> 30</span>
+                  <span className="stat"><i className="glyphicon glyphicon-heart" /> 901</span>
                 </span>
                 <i className="glyphicon glyphicon-paperclip" />
               </div>
             </div>
             <div className="item-user">
-              <img src="http://lorempixel.com/user/50/50" alt="" />
-              <a>John Doe</a>
+              <a className="user-link">
+                <img className="user-avatar" src="https://media.giphy.com/avatars/comedycentral/XCQBE9EqWbb2.jpg" alt="" />
+                <span>John Doe</span>
+              </a>
             </div>
           </div>
-          <div className="col-md-3 image-item">
+          <div className="col-xs-6 col-sm-4 col-md-3 image-item">
             <div className="item-content">
-              <img src="http://lorempixel.com/100/100" alt="" />
+              <div className="item-img" style={{ backgroundImage: 'url(https://media2.giphy.com/media/bbs0P60ks0AZ1zOUhx/100.gif)' }} />
               <div className="item-stat">
                 <span className="pull-right">
-                  <span><i className="glyphicon glyphicon-eye-open" /> 7,693</span>
-                  <span><i className="glyphicon glyphicon-comment" /> 30</span>
-                  <span><i className="glyphicon glyphicon-heart" /> 901</span>
+                  <span className="stat"><i className="glyphicon glyphicon-eye-open" /> 7,693</span>
+                  <span className="stat"><i className="glyphicon glyphicon-comment" /> 30</span>
+                  <span className="stat"><i className="glyphicon glyphicon-heart" /> 901</span>
                 </span>
                 <i className="glyphicon glyphicon-paperclip" />
               </div>
             </div>
             <div className="item-user">
-              <img src="http://lorempixel.com/user/50/50" alt="" />
-              <a>John Doe</a>
+              <a className="user-link">
+                <img className="user-avatar" src="https://media.giphy.com/avatars/comedycentral/XCQBE9EqWbb2.jpg" alt="" />
+                <span>John Doe</span>
+              </a>
             </div>
           </div>
-          <div className="col-md-3 image-item">
+          <div className="col-xs-6 col-sm-4 col-md-3 image-item">
             <div className="item-content">
-              <img src="http://lorempixel.com/100/100" alt="" />
+              <div className="item-img" style={{ backgroundImage: 'url(https://media2.giphy.com/media/bbs0P60ks0AZ1zOUhx/100.gif)' }} />
               <div className="item-stat">
                 <span className="pull-right">
-                  <span><i className="glyphicon glyphicon-eye-open" /> 7,693</span>
-                  <span><i className="glyphicon glyphicon-comment" /> 30</span>
-                  <span><i className="glyphicon glyphicon-heart" /> 901</span>
+                  <span className="stat"><i className="glyphicon glyphicon-eye-open" /> 7,693</span>
+                  <span className="stat"><i className="glyphicon glyphicon-comment" /> 30</span>
+                  <span className="stat"><i className="glyphicon glyphicon-heart" /> 901</span>
                 </span>
                 <i className="glyphicon glyphicon-paperclip" />
               </div>
             </div>
             <div className="item-user">
-              <img src="http://lorempixel.com/user/50/50" alt="" />
-              <a>John Doe</a>
+              <a className="user-link">
+                <img className="user-avatar" src="https://media.giphy.com/avatars/comedycentral/XCQBE9EqWbb2.jpg" alt="" />
+                <span>John Doe</span>
+              </a>
             </div>
           </div>
-          <div className="col-md-3 image-item">
+          <div className="col-xs-6 col-sm-4 col-md-3 image-item">
             <div className="item-content">
-              <img src="http://lorempixel.com/100/100" alt="" />
+              <div className="item-img" style={{ backgroundImage: 'url(https://media2.giphy.com/media/bbs0P60ks0AZ1zOUhx/100.gif)' }} />
               <div className="item-stat">
                 <span className="pull-right">
-                  <span><i className="glyphicon glyphicon-eye-open" /> 7,693</span>
-                  <span><i className="glyphicon glyphicon-comment" /> 30</span>
-                  <span><i className="glyphicon glyphicon-heart" /> 901</span>
+                  <span className="stat"><i className="glyphicon glyphicon-eye-open" /> 7,693</span>
+                  <span className="stat"><i className="glyphicon glyphicon-comment" /> 30</span>
+                  <span className="stat"><i className="glyphicon glyphicon-heart" /> 901</span>
                 </span>
                 <i className="glyphicon glyphicon-paperclip" />
               </div>
             </div>
             <div className="item-user">
-              <img src="http://lorempixel.com/user/50/50" alt="" />
-              <a>John Doe</a>
+              <a className="user-link">
+                <img className="user-avatar" src="https://media.giphy.com/avatars/comedycentral/XCQBE9EqWbb2.jpg" alt="" />
+                <span>John Doe</span>
+              </a>
             </div>
           </div>
         </div>
